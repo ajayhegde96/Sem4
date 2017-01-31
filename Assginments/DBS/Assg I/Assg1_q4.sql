7ee3df31b84e0788e659950f958088ded34207e3
Akashraj <akashrajr1@gmail.com>
2017-01-31 19:40:21 +0530

    Q4 of Dbs AssgI

---
+++ Assginments/DBS/Assg I/Assg1_q4.sql
@@ -0,0 +1,7 @@
+select aname from author natural join writes natural join book group by aname having count(distinct title)>1;
+
+select title from book natural join writes natural join author group by title having count(distinct aname)=1;
+
+select anme from author natural join writes natural join author group by aname having max(count(distinct title));
+
+select pname from publisher natural join book group by category having max(count(category));
