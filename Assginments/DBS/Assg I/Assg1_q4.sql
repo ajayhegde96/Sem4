525911dc47ca7f1fff8b54dec2c2709c91018aca
ajayhegde96 <ajayhegde97@gmail.com>
2017-02-02 09:37:57 +0530

    Update Assg1_q4.sql

--- Assginments/DBS/Assg I/Assg1_q4.sql
+++ Assginments/DBS/Assg I/Assg1_q4.sql
@@ -2,6 +2,6 @@ select aname from author natural join writes natural join book group by aname ha
 
 select title from book natural join writes natural join author group by title having count(distinct aname)=1;
 
-select anme from author natural join writes natural join author group by aname having max(count(distinct title));
+select aname from author natural join writes natural join author group by aname having max(count(distinct title));
 
 select pname from publisher natural join book group by category having max(count(category));
